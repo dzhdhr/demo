34fa7683c8814e2c1858d0c123afdb0e65f2ff18
eric <dongzihao123@126.com>
2022-08-07 12:53:20 -0700

    init

--- admin/src/main.js
+++ admin/src/main.js
@@ -2,13 +2,15 @@ import { createApp } from 'vue'
 import {createRouter,createWebHistory} from "vue-router/dist/vue-router";
 import App from './App.vue'
 import LogIn from "@/view/Login";
+import adminPage from "@/view/admin";
 
 const router = createRouter(
     {
         history:createWebHistory(),
         routes:[
             {path:'/login', component:LogIn},
-            {path:'/',redirect:"/login"}
+            {path:'/',redirect:"/login"},
+            {path:"/admin",component:adminPage}
         ]
     }
 )
